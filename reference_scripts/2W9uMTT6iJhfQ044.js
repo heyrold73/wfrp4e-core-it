5feea8cc67a75437c2fc5e465feaf42a89d90f5e
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2024-12-17 20:59:47 +0100

    Add files via upload

---
+++ reference_scripts/2W9uMTT6iJhfQ044.js
@@ -0,0 +1,19 @@
+let skill = `Trade (${this.item.parenthesesText})`
+let currentCareer = this.actor.system.currentCareer;
+let existingSkill = this.actor.itemTypes.skill.find(i => i.name == skill);
+
+if (!currentCareer) return
+
+
+let inCurrentCareer = currentCareer.system.skills.includes(skill);
+let craftsmanAdded = this.actor.getFlag("wfrp4e", "craftsmanAdded") || {};
+if (existingSkill && inCurrentCareer && !craftsmanAdded[existingSkill.name])
+{
+	existingSkill.system.advances.costModifier = -5;
+}
+else 
+{
+	craftsmanAdded[skill] = true;
+	currentCareer.system.skills.push(skill);
+	setProperty(this.actor, "flags.wfrp4e.craftsmanAdded", craftsmanAdded)
+}
