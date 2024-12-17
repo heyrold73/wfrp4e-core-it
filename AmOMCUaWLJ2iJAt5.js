ece9d7431a21be04634b47dd5b70b8d06e0d14b4
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2024-12-17 21:59:05 +0100

    Delete AmOMCUaWLJ2iJAt5.js

--- AmOMCUaWLJ2iJAt5.js
+++
@@ -1,3 +0,0 @@
-let item = await fromUuid("Compendium.wfrp4e-core.items.IFKWu98qmWpaSfUi")
-let data = item.toObject();
-this.actor.createEmbeddedDocuments("Item", [data], {fromEffect : this.effect.id})
