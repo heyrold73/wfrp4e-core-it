2efff92b36fc4395149f4ce63086b7104bc07774
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2024-12-17 22:06:06 +0100

    Delete a7pEvCycVnFBXGAQ.js

--- a7pEvCycVnFBXGAQ.js
+++
@@ -1,7 +0,0 @@
-let test = await this.actor.setupCharacteristic("i", {skipTargets: true, appendTitle :  " - Stunned", fields : {difficulty : "easy"}})
-await test.roll();
-
-if (!test.succeeded)
-{
-	this.actor.addCondition("stunned");
-}
