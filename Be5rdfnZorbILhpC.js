0589c107d9262555b4a693f92480468a3182dcdb
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2024-12-17 22:02:48 +0100

    Delete Be5rdfnZorbILhpC.js

--- Be5rdfnZorbILhpC.js
+++
@@ -1,4 +0,0 @@
-let item = await fromUuid("Compendium.wfrp4e-core.items.pLW9SVX0TVTYPiPv")
-let data = item.toObject();
-data.system.specification.value = 3 - this.actor.characteristics.s.bonus
-this.actor.createEmbeddedDocuments("Item", [data], {fromEffect : this.effect.id})
