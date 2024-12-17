b4342bac45ae9863480dae66ce75c6e89e3a744e
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2024-12-17 21:57:57 +0100

    Delete AWgZnm9xLyIWCDnp.js

--- AWgZnm9xLyIWCDnp.js
+++
@@ -1,9 +0,0 @@
-let robust = args.modifiers.other.find(i => i.key == "robust")
-if (robust)
-{
-	robust.value--;
-}
-else 
-{
-	args.modifiers.other.push({key: "robust", label : this.effect.name, value : -1})
-}
