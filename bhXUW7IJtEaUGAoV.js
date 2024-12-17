3d645795da6c81173e51432a8ba3dc2ad36ba988
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2024-12-17 22:11:49 +0100

    Delete bhXUW7IJtEaUGAoV.js

--- bhXUW7IJtEaUGAoV.js
+++
@@ -1,7 +0,0 @@
-let test = await this.actor.setupSkill(game.i18n.localize("NAME.Endurance"), {skipTargets: true, appendTitle :  ` - ${this.effect.name}`})
-await test.roll();  
-if (test.failed)
-{
-    this.actor.addCondition("prone");
-}
-  
