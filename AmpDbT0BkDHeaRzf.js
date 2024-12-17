3112bd080b97c46cb905eb70513bc36036322e39
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2024-12-17 21:59:14 +0100

    Delete AmpDbT0BkDHeaRzf.js

--- AmpDbT0BkDHeaRzf.js
+++
@@ -1,4 +0,0 @@
-let item = await fromUuid("Compendium.wfrp4e-core.items.Item.pTorrE0l3VybAbtn");
-let data = item.toObject();
-data.system.specification.value = 1;
-this.actor.createEmbeddedDocuments("Item", [data], {fromEffect : this.effect.id})
