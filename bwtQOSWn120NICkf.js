6c136d80886a200b76c155fdd9b1fa4ab64819b4
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2024-12-17 22:12:16 +0100

    Delete bwtQOSWn120NICkf.js

--- bwtQOSWn120NICkf.js
+++
@@ -1,8 +0,0 @@
-let test = await this.actor.setupCharacteristic("ag", {fields : {difficulty : "hard"}});
-await test.roll();
-
-if (test.failed)
-{
-   await this.actor.addCondition("bleeding")
-   await this.actor.addCondition("entangled")
-}
